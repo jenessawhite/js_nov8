40cbcb4f1c1fbc433460eb79f855101f10622a3a
Jenessa <jenessaw31@gmail.com>
2016-11-08 14:02:17 -0600

    Palindrome completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('hey now');
+// Write a function isPalindrome(x)
+// that returns true if x is a palindrome,
+// otherwise false.
+//
+// Palindromes are words that are the same
+// going forwards and backwards. Examples:
+//
+// i
+// dod
+// meeteem
+// TrickirT
+
+function isPalindrome(str){
+  //start with everything passes
+  if (typeof str === 'undefined') { //if nothing in string then it passes
+    return true;
+  } else { //seperate those that will NOT pass
+    //reverse the string
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString = newString + str[i];
+    }
+    //does the string reversal equate to the reg str?
+      if (newString !== str) {
+        return false;
+      }
+    }
+
+  return true; //returns those that ARE palindromes
+}
+
+// tests
+
+console.assert( isPalindrome("tacocat") === true )
+console.assert( isPalindrome("Tacocat") === false )
+console.assert( isPalindrome("racecar") === true )
+console.assert( isPalindrome("cowboy") === false )
